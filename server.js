d9024dfb0dddab9eea2754b7457003db94fea164
Joe Hand <joe@joeahand.com>
2016-09-14 10:13:20 -0600

    actually add server

---
+++ server.js
@@ -0,0 +1,20 @@
+const browserify = require('browserify')
+const bankai = require('bankai')
+const http = require('http')
+const path = require('path')
+
+const client = path.join(__dirname, 'index.js')
+
+const assets = bankai({optimize: true})
+const css = assets.css()
+const js = assets.js(browserify, client, {transform: ['es2040']})
+const html = assets.html()
+
+http.createServer((req, res) => {
+  switch (req.url) {
+    case '/': return html(req, res).pipe(res)
+    case '/bundle.js': return js(req, res).pipe(res)
+    case '/bundle.css': return css(req, res).pipe(res)
+    default: return res.statusCode = 404 && res.end('404 not found')
+  }
+}).listen(8080)
